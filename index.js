3f808959f51a7fe4f5287e0dd25e30bd81eb61a6
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-07-01 07:37:59 +0600

    Upto 58:00, included CRUD operations

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+// init middleware
+// app.use(logger);
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api routes
+app.use('/api/members', require('./routes/api/members'))
+
+
 
 const PORT = process.env.PORT || 3000;
 
